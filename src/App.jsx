3daf03af815415d0689e5c2921e1b3e5a448705c
Its-Yasir <iamyasirtariq@gmail.com>
2025-09-05 21:15:55 +0500

    completed ui of specs input

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,17 @@ import Particles from './components/reactbits/Particles';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './Home';
 import GeminiGenerator from './GeminiGenerator';
+import { useState} from 'react';
 
 function App() {
+
+  const [cpuOptionSelected, setCpuOptionSelected] = useState('intel');
+  const [gpuOptionSelected, setGpuOptionSelected] = useState('nvidia');
+  const [ramOptionUnitSelected, setOptionRamUnitSelected] = useState('gb');
+  const [ramOptionTypeSelected, setOptionRamTypeSelected] = useState('ddr3');
+  const [storageOptionUnitSelected, setStorageOptionUnitSelected] = useState('gb');
+  const [storageOptionTypeSelected, setStorageOptionTypeSelected] = useState('hdd');
+
   return (
     <Router>
       <Routes>
@@ -44,6 +53,50 @@ function App() {
         />
         <Route
           path="/specs"
+          element={
+            <div style={{ position: 'relative', width: '100%', height: '100vh' }}>
+              {/* Background Particles */}
+              <Particles
+                particleColors={['#ffffff', '#ffffff']}
+                particleCount={200}
+                particleSpread={10}
+                speed={0.1}
+                particleBaseSize={100}
+                moveParticlesOnHover={true}
+                alphaParticles={false}
+                disableRotation={false}
+              />
+
+              {/* Foreground content with ClickSpark */}
+              <ClickSpark
+                sparkColor="#26bbff"
+                sparkSize={20}
+                sparkRadius={25}
+                sparkCount={10}
+                duration={400}
+              >
+                <div style={{ position: 'relative', zIndex: 1 }}>
+                  <Specs
+                    cpuOptionSelected={cpuOptionSelected}
+                    setCpuOptionSelected={setCpuOptionSelected}
+                    gpuOptionSelected={gpuOptionSelected}
+                    setGpuOptionSelected={setGpuOptionSelected}
+                    ramOptionUnitSelected={ramOptionUnitSelected}
+                    setOptionRamUnitSelected={setOptionRamUnitSelected}
+                    ramOptionTypeSelected={ramOptionTypeSelected}
+                    setOptionRamTypeSelected={setOptionRamTypeSelected}
+                    storageOptionUnitSelected={storageOptionUnitSelected}
+                    setStorageOptionUnitSelected={setStorageOptionUnitSelected}
+                    storageOptionTypeSelected={storageOptionTypeSelected}
+                    setStorageOptionTypeSelected={setStorageOptionTypeSelected}
+                  />
+                </div>
+              </ClickSpark>
+            </div>
+          }
+        />
+        <Route
+          path="/get-started"
           element={
             <div style={{ position: 'relative', width: '100%', height: '100vh' }}>
               {/* Background Particles */}
--- src/Specs.jsx
+++ src/Specs.jsx
@@ -1,10 +1,144 @@
 import './Specs.css';
+import { GiCpu } from "react-icons/gi";
+import { BsGpuCard } from "react-icons/bs";
+import { IoArrowBack } from "react-icons/io5";
+import { FaArrowRight } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
 
-function Specs() {
+
+function Specs({
+  cpuOptionSelected,
+  gpuOptionSelected,
+  ramOptionUnitSelected,
+  ramOptionTypeSelected,
+  storageOptionUnitSelected,
+  setCpuOptionSelected,
+  setGpuOptionSelected,
+  setOptionRamUnitSelected,
+  setOptionRamTypeSelected,
+  setStorageOptionUnitSelected,
+  setStorageOptionTypeSelected,
+  storageOptionTypeSelected
+}) {
+  const [cpus, setCpus] = useState([]);
+  const [cpuSearch, setCpuSearch] = useState("");
+  useEffect(() => {
+    fetch("/cpus_with_a10.json") // put file in /public/
+      .then(res => res.json())
+      .then(data => {
+        if(cpuOptionSelected==="intel"){
+          setCpus(data.intel)
+        }else if(cpuOptionSelected==="amd"){
+          setCpus(data.amd)
+        }else if(cpuOptionSelected==="apple"){
+          setCpus(data.apple)
+        }
+      });
+  }, [cpuOptionSelected]);
   return (
     <div className="specs-container">
-      <h1>PC Specifications</h1>
-      <p>Here you can add your PC specifications.</p>
+      <h1>
+        <GiCpu />
+        Add PC Specifications
+        <BsGpuCard />
+      </h1>
+      <div className="CPU">
+        <h1>CPU:</h1>
+        <div className='cpu-inputs'>
+          <input type="text" placeholder='Enter CPU Name' value={cpuSearch} onChange={(e) => setCpuSearch(e.target.value)} />
+          <div className="cpuoptionsSearched optionsSearched">
+            {cpus.filter(cpu => cpu.toLowerCase().includes(cpuSearch.toLowerCase())).map((cpu, index) => (
+              <div key={index} className="optionSearched"
+              onClick={(e) => {setCpuSearch(e.target.textContent);}}
+              >{cpu}</div>
+            ))}
+          </div>
+          <select
+            value={cpuOptionSelected}
+            onChange={(e) => setCpuOptionSelected(e.target.value)}
+          >
+            <option value="intel" selected>Intel</option>
+            <option value="amd">AMD</option>
+            <option value="apple">Apple</option>
+          </select>
+        </div>
+      </div>
+      <div className="GPU">
+        <h1>GPU:</h1>
+        <div className='gpu-inputs'>
+          <input type="text" placeholder='Enter GPU Name' />
+          <div className="gpuoptionsSearched optionsSearched">
+            <div className="optionSearched">Intel i3</div>
+            <div className="optionSearched">Intel i3</div>
+            <div className="optionSearched">Intel i3</div>
+            <div className="optionSearched">Intel i3</div>
+          </div>
+          <select
+            value={gpuOptionSelected}
+            onChange={(e) => setGpuOptionSelected(e.target.value)}
+          >
+            <option value="nvidia" selected>NVIDIA</option>
+            <option value="intel">Intel</option>
+            <option value="amd">AMD</option>
+            <option value="asus">ASUS</option>
+          </select>
+        </div>
+      </div>
+      <div className="RAM">
+        <h1>RAM:</h1>
+        <div className='ram-inputs'>
+          <input type="number" placeholder='Enter RAM' />
+          <select
+            value={ramOptionUnitSelected}
+            onChange={(e) => setOptionRamUnitSelected(e.target.value)}
+          >
+            <option value="gb" selected>GB</option>
+            <option value="mb">MB</option>
+          </select>
+          <select
+            value={ramOptionTypeSelected}
+            onChange={(e) => setOptionRamTypeSelected(e.target.value)}
+          >
+            <option value="ddr3" selected>DDR3</option>
+            <option value="ddr4">DDR4</option>
+            <option value="ddr5">DDR5</option>
+          </select>
+        </div>
+      </div>
+      <div className="Storage">
+        <h1>FREE Storage:</h1>
+        <div className='storage-inputs'>
+          <input type="number" placeholder='Enter Free Storage' />
+          <select
+            value={storageOptionUnitSelected}
+            onChange={(e) => setStorageOptionUnitSelected(e.target.value)}
+          >
+            <option value="gb" selected>GB</option>
+            <option value="mb">MB</option>
+          </select>
+          <select
+            value={storageOptionTypeSelected}
+            onChange={(e) => setStorageOptionTypeSelected(e.target.value)}
+          >
+            <option value="hdd" selected>HDD</option>
+            <option value="ssd">SSD</option>
+          </select>
+        </div>
+      </div>
+      <div className="buttons">
+        <Link to={"/"}>
+          <button className="go-back">
+            <IoArrowBack className='icon' />
+            Go Back
+          </button>
+        </Link>
+        <Link to={"/get-started"}>
+          <button className="add-specs">Add Specs
+            <FaArrowRight />
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
